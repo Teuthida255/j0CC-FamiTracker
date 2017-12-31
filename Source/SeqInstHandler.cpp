e031b1e2e852ad06a19fdbfb0d9ae16087ae93ba
jimbo1qaz <jimbo1qaz@gmail.com>
2017-12-31 00:04:19 -0800

    Fix memory corruption entering MML volume sequences over 252 (MAX_SEQUENCE_ITEMS) items long.

--- Source/SeqInstHandler.cpp
+++ Source/SeqInstHandler.cpp
@@ -95,13 +95,14 @@ void CSeqInstHandler::UpdateInstrument()
 	if (!m_pInterface->IsActive()) return;
 	for (size_t i = 0; i < std::size(m_pSequence); i++) {
 		if (m_pSequence[i] == nullptr || m_pSequence[i]->GetItemCount() == 0) continue;
-		int Value = m_pSequence[i]->GetItem(m_iSeqPointer[i]);
+		int Value;
 		switch (m_iSeqState[i]) {
 		case SEQ_STATE_RUNNING:
+			Value = m_pSequence[i]->GetItem(m_iSeqPointer[i]);
 			ProcessSequence(i, m_pSequence[i]->GetSetting(), Value);
+			++m_iSeqPointer[i];
 
-			{
-				++m_iSeqPointer[i];
+			{	
 				int Release = m_pSequence[i]->GetReleasePoint();
 				int Items = m_pSequence[i]->GetItemCount();
 				int Loop = m_pSequence[i]->GetLoopPoint();
